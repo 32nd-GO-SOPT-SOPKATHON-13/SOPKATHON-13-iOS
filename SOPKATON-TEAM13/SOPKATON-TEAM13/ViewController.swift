3c4736e8e0e60923cd4ca0effc15afcb2ac5e613
kwonjeong <kwonjeong9470@gmail.com>
2023-05-18 23:40:36 +0900

    [Setting] #1 - 스토리보드 제거

---
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPKATON-TEAM13
+//
+//  Created by KJ on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
