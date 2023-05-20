1de560feb97425297644cca77108d846d194b2fc
PecanPie <hidra0321@naver.com>
2023-05-20 23:08:21 +0900

    Added Font Extension method

--- SOPKATON-TEAM13/SOPKATON-TEAM13/Utils/Extension/UIFont+.swift
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/Utils/Extension/UIFont+.swift
@@ -5,4 +5,25 @@
 //  Created by KJ on 2023/05/18.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    static func appleSDGothic(weightOf weight: AppleSDGothicFamily, sizeOf size: AppleSDGothicFontSize) -> UIFont? {
+        return UIFont(name: "AppleSDGothicNeo-\(weight.rawValue)", size: size.rawValue)
+    }
+    
+    enum AppleSDGothicFamily: String {
+        case Regular, Bold
+    }
+    
+    enum AppleSDGothicFontSize: CGFloat {
+        case font12 = 12
+        case font13
+        case font14
+        case font16 = 16
+        case font18 = 18
+        case font20 = 20
+        case font22 = 22
+        case font24 = 24
+    }
+}
