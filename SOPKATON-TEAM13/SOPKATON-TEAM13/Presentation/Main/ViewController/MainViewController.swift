aeb7df48bbe5ea1ce94ed1122975eacdb29478fb
PecanPie <hidra0321@naver.com>
2023-05-21 03:32:39 +0900

    Configured MainViewController

--- SOPKATON-TEAM13/SOPKATON-TEAM13/Presentation/Main/ViewController/MainViewController.swift
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/Presentation/Main/ViewController/MainViewController.swift
@@ -7,23 +7,84 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+import SnapKit
+import Then
 
+class MainViewController: UIViewController {
+    
+    private lazy var buttonWidth = UIScreen.main.bounds.width / 3.15
+    private lazy var buttonHeight = buttonWidth * 1.19
+    
+    private let upperHouseView = neighborButtonView(addressNumber: 1402, pokedCount: 999)
+    private let lowerHouseView = neighborButtonView(addressNumber: 1402, pokedCount: 999)
+    private let leftHouseView = neighborButtonView(addressNumber: 1402, pokedCount: 999)
+    private let rightHouseView = neighborButtonView(addressNumber: 1402, pokedCount: 999)
+    
+    private let todayPokedCount = UILabel()
+    private let mainCharacterImageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension MainViewController {
+    
+    private func setUI() {
+        view.backgroundColor = .white
+        
+        configureCell(999)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setLayout() {
+        view.addSubviews(todayPokedCount, upperHouseView, lowerHouseView, leftHouseView, rightHouseView)
+        
+        todayPokedCount.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.centerX.equalToSuperview()
+        }
+        
+        upperHouseView.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(-buttonHeight - 5)
+            $0.height.equalTo(buttonHeight)
+            $0.width.equalTo(buttonWidth)
+            $0.centerX.equalToSuperview()
+        }
+        
+        lowerHouseView.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(buttonHeight + 5)
+            $0.height.equalTo(buttonHeight)
+            $0.width.equalTo(buttonWidth)
+            $0.centerX.equalToSuperview()
+        }
+        
+        leftHouseView.snp.makeConstraints {
+            $0.height.equalTo(buttonHeight)
+            $0.width.equalTo(buttonWidth)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        rightHouseView.snp.makeConstraints {
+            $0.height.equalTo(buttonHeight)
+            $0.width.equalTo(buttonWidth)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
     }
-    */
+}
 
+extension MainViewController {
+    func configureCell(_ data: Int) {
+        
+        todayPokedCount.text = "오늘 나는 \(data) 번 찔렸어요 🥹"
+        let fullText = todayPokedCount.text ?? ""
+        let attribtuedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: "\(data)")
+        attribtuedString.addAttribute(.foregroundColor, value: UIColor.MainColor, range: range)
+        attribtuedString.addAttribute(.font, value: UIFont.appleSDGothic(weightOf: .Bold, sizeOf: .font24) ?? UIFont(), range: range)
+        todayPokedCount.attributedText = attribtuedString
+    }
 }
--- SOPKATON-TEAM13/SOPKATON-TEAM13/Utils/Extension/UIButton+.swift
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/Utils/Extension/UIButton+.swift
@@ -18,4 +18,16 @@ extension UIButton {
             )
             setAttributedTitle(attributedString, for: .normal)
         }
+    
+    func setBackgroundColor(color: UIColor) {
+        self.clipsToBounds = true  // add this to maintain corner radius
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.setBackgroundImage(colorImage, for: .highlighted)
+        }
+    }
 }
