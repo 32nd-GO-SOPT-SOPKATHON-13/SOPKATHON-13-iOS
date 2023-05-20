b74d2cd0b693decadc47134293bdcf85fd85fb50
kwonjeong <kwonjeong9470@gmail.com>
2023-05-21 04:49:29 +0900

    [Feat] #18 - NoticeResponse 추가

--- SOPKATON-TEAM13/SOPKATON-TEAM13/Data/DTO/Notice/NoticeResponse.swift
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/Data/DTO/Notice/NoticeResponse.swift
@@ -5,4 +5,21 @@
 //  Created by KJ on 2023/05/20.
 //
 
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let contentResponse = try? JSONDecoder().decode(ContentResponse.self, from: jsonData)
+
 import Foundation
+
+// MARK: - ContentResponse
+struct ContentResponse: Codable {
+    let code: Int
+    let message: String
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let averageCount, complainedDays, complainedCount: Int
+}
