31e6beb60fb561f38c965b0adc40daea9814edd4
kwonjeong <kwonjeong9470@gmail.com>
2023-05-21 00:37:50 +0900

    [Feat] #11 - Navigation Bar 추가

--- SOPKATON-TEAM13/SOPKATON-TEAM13/Presentation/Notice/ViewController/NoticeViewController.swift
+++ SOPKATON-TEAM13/SOPKATON-TEAM13/Presentation/Notice/ViewController/NoticeViewController.swift
@@ -11,7 +11,7 @@ import Alamofire
 import SnapKit
 import Then
 
-final class ViewController: UIViewController {
+final class NoticeViewController: UIViewController {
     
     // MARK: - UI Components
     
@@ -29,14 +29,17 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setNavigationBar()
     }
 }
 
-extension ViewController {
+extension NoticeViewController {
     
     // MARK: - UI Components Property
     
     private func setUI() {
+        
+        view.backgroundColor = Color.white
         
     }
     
@@ -47,6 +50,32 @@ extension ViewController {
     }
     
     // MARK: - Methods
+    
+    private func setNavigationBar() {
+//        navigationController?.navigationBar.tintColor = Color.white
+        navigationController?.navigationBar.backgroundColor = Color.white
+//        let buttonInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8) // 원하는 여백 값으로 수정
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(
+//            image: Image.carrotBack,
+//            style: .plain,
+//            target: self,
+//            action: #selector(backButtonTapped)
+//        )
+        
+        let title = "소음고지서"
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: Color.gray8,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .bold)
+        ]
+        
+        if let titleLabel = navigationItem.titleView as? UILabel {
+            titleLabel.attributedText = NSAttributedString(string: title, attributes: attributes)
+        } else {
+            let titleLabel = UILabel()
+            titleLabel.attributedText = NSAttributedString(string: title, attributes: attributes)
+            navigationItem.titleView = titleLabel
+        }
+    }
     
     // MARK: - @objc Methods
 }
